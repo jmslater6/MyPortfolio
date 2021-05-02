a01e03ea76e81bec605ef6768f8244f0d4060962
jmslater6 <76633385+jmslater6@users.noreply.github.com>
2021-05-01 21:51:37 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(documnet).ready(function(){
+  activeLinkControl()
+
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navlist");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function activeLinkControl() {
+  $('.navlist .li a').click(function() {
+    //remove active class
+    $('.li').removeClass('active')
+    // add active class to clicked item
+    $(this).closest('.li').addClass('active')
+  })
+}
